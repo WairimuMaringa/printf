772851161b46444048dc73724733f6c6452e9d6a
WairimuMaringa <makena96wairimu@gmail.com>
2022-07-09 12:46:26 -0700

    first prototype added

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/* Libraries */
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Prototypes */
+int _printf(const char *format, ...);
