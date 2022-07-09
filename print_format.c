7d6fde0ab6c62a2e0ed9eaa96d0bd8feb5fdf187
Bruce <jacobbruce880@gmail.com>
2022-07-09 13:07:07 -0700

    Prints characters based on format specifiers

---
+++ print_format.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _printf - prints to output based on fomart
+ * @format: character arrsy
+ *
+ * Return: Returns number of printed characters
+ */
+int _printf(const char *format, ...)
+{
+
