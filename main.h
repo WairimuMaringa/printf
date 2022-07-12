5a769788fe6b9516d932a674d7f1aeaf9e911749
Bruce <jacobbruce880@gmail.com>
2022-07-12 06:38:56 -0700

    Fixes compile issues

--- main.h
+++ main.h
@@ -1,21 +1,8 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-/* Libraries */
 #include <stdarg.h>
-
-/**
- * struct print - print various conversion specifiers
- * @print: type to print
- * @p: print the function specified
- */
-typedef struct printthis
-{
-	char *print;
-	int (*p)(va_list);
-} print_t;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_char(char count, va_list list);
 #endif
