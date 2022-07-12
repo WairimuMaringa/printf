339191bef48fa990528ae752939264bf8ce64db0
Bruce <jacobbruce880@gmail.com>
2022-07-12 10:27:16 -0700

    Printf-like function

--- main.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-int main()
-{
-	_printf("Let's try to printf a simple sentence.\n");
-	 _printf("Character:[%c]\n", 'H');
-	   _printf("String:[%s]\n", "I am a string !");
-	return 0;
-}
--- printf.c
+++ printf.c
@@ -1,16 +1,23 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 int _printf(const char *format, ...)
 {
-	int len, per, count, nprint;
+	int len, per, count, nprint, digits, temp;
 
 	char *token;
 
+	int nums;
+
 	va_list list;
+
+	char str[8];
 
 	va_start(list, format);
 
 	nprint = 0;
+	digits = 0;
+	count = 0;
 	for (len = 0; format[len] != '\0'; len++)
 	{}
 	for (per = 0; format[per] != '%'; per++)
@@ -19,6 +26,35 @@ int _printf(const char *format, ...)
 	{
 		switch(format[per + 1])
 		{
+			case 'd':
+			{
+				for (count = 0; count < per; count++)
+				{
+					_putchar(format[count]);
+					nprint++;
+				}
+				nums = va_arg(list, int);
+				temp = nums;
+				while (nums != 0)
+				{
+					nums = nums / 10;
+					digits++;
+				}
+				while (count <= digits)
+				{
+					str[count] = (temp % 10);
+					_putchar(str[count]);
+					temp = ((temp - str[count]) / 10);
+					nprint++;
+					count++;
+				}
+				for (count = per + 2; format[count] != '\0'; count++)
+				{
+					_putchar(format[count]);
+				}
+				nprint--;
+			}
+			break;
 			case 's':
 			{
 				for (count = 0;  count < per; count++)
