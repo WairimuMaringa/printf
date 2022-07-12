bfbd2dd856a574ad7e14d5b02c89bd4173ba7f42
WairimuMaringa <makena96wairimu@gmail.com>
2022-07-11 21:17:30 -0700

    another printf version

--- main.c
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-int main()
-{
-_printf("Negative:[%d]\n", -762534);
-return 0;
-}
--- main.h
+++ main.h
@@ -12,6 +12,20 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 
 
+/**
+ * struct print - print various conversion specifiers
+ * @print: type to print
+ * @p: print the function specified
+ */
+typedef struct print
+{
+	char *print;
+	int (*p)(va_list);
+} print_t;
+
+
+
+
 
 
 #endif
